eccf01b22692a7fa74a4dd50b84e0a9e3910f102
tvvmvn <tvvmvn@gmail.com>
2023-07-26 11:58:33 +0900

    7/26

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -2,6 +2,44 @@ const User = require('../models/User');
 const Follow = require('../models/Follow');
 const Article = require('../models/Article');
 
+exports.update = [
+  fileHandler('profiles').single('avatar'),
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        const err = new Error();
+        err.errors = errors.array();
+        err.status = 400;
+        throw err;
+      }
+
+      const _user = req.user;
+
+      if (req.file) {
+        _user.avatar = req.file.filename;
+      }
+
+      Object.assign(_user, req.body);
+
+      await _user.save();
+
+      const user = {
+        fullName: _user.fullName,
+        avatar: _user.avatar,
+        bio: _user.bio,
+        token
+      }
+
+      res.json({ user })
+
+    } catch (error) {
+      next(error)
+    }
+  }
+]
+
 exports.find = async (req, res, next) => {
   try {
 
